4d0adf349654add72254a8fb5568120a76fca678
wali <bagader111@outlook.com>
2023-02-13 17:02:47 +0800

    add form page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Sidebar from "./scenes/global/Sidebar";
 import Team from "./scenes/team";
 import Contacts from "./scenes/contacts";
 import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -23,6 +24,7 @@ function App() {
               <Route path="/team" element={<Team />} />
               <Route path="/contacts" element={<Contacts />} />
               <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
             </Routes>
           </main>
         </div>
--- src/scenes/invoices/index.jsx
+++ src/scenes/invoices/index.jsx
@@ -80,3 +80,4 @@ const Invoices = () => {
 };
 
 export default Invoices;
+ 
