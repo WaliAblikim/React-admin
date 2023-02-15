72d514cdfd5a452cf7fecbb1354cf2eccdbc8730
wali <bagader111@outlook.com>
2023-02-16 00:51:46 +0800

    add dashboard page

--- src/components/GeographyChart.jsx
+++ src/components/GeographyChart.jsx
@@ -1,8 +1,8 @@
-import { ResponsiveChoropleth } from "@nivo/geo";
 import { useTheme } from "@mui/material";
+import { ResponsiveChoropleth } from "@nivo/geo";
+import { geoFeatures } from "../data/mockGeoFeatures";
 import { tokens } from "../theme";
 import { mockGeographyData as data } from "../data/mockData";
-import { geoFeatures } from "../data/mockGeoFeatures";
 
 const GeographyChart = ({ isDashboard = false }) => {
   const theme = useTheme();
